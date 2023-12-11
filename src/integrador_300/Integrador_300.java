8d91aa320b6fcbc4980b0c948c2ad896e9e78082
Ismael Antonio <kj34157373@gmail.com>
2023-12-10 23:01:56 -0600

    Avance del proyecto integrador

---
+++ src/integrador_300/Integrador_300.java
@@ -0,0 +1,16 @@
+
+package integrador_300;
+
+public class Integrador_300 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmCostos ventana = new frmCostos();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+    }
+    
+}
